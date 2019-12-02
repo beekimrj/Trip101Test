4bf1ba3603041ce905456a62ca58990b8d004eaf
beekimrj <beekimrj@gmail.com>
2019-12-02 22:49:11 +0545

    add feature to check even social medias in 'lets connect' section

--- cypress/integration/Article/letsConnect_spec.js
+++ cypress/integration/Article/letsConnect_spec.js
@@ -5,6 +5,19 @@ describe('Article', () => {
     })
     it('check for lets connect in article page', () => {
        cy.get('article.sidebar.widget.text-center > h4').contains("Let's connect")
+
+       const socialmedia = [
+        { SRC: "facebook", index: 1 },
+        { SRC: "twitter", index: 2 },
+        { SRC: "instagram", index: 3 },
+        { SRC: "pinterest", index: 4 },
+    ]
+
+    cy.wrap(socialmedia).each((item) => {
+        cy.get(`.social-follow > a:nth-child(${item.index})`)
+            .should('have.attr', 'href').and('contains', `${item.SRC}`)
+
+    })
     });
 
 })
