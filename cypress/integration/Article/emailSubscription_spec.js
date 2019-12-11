e1a6b1b6022de4f91ddd8433a8901d7ee29fd156
beekimrj <beekimrj@gmail.com>
2019-12-11 12:08:07 +0545

    Merge all files of article test, for now test Email Subscription and searchBox related task like powered by and lazy load of Car rentals and flight

--- cypress/integration/Article/emailSubscription_spec.js
+++ cypress/integration/Article/emailSubscription_spec.js
@@ -1,33 +1,32 @@
-//makeid generates random text for email, i did it because it will not be possible to change 
-//email everytime we make a test
 
-function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
- }
-
- describe('Email', () => {
+describe('Article', () => {
     before(() => {
         cy.visit('/article/what-to-buy-in-kathmandu')
-       
     });
 
+
     context('Email subscription', () => {
+
+        function makeid(length) {
+            var result = '';
+            var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+            var charactersLength = characters.length;
+            for (var i = 0; i < length; i++) {
+                result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            }
+            return result;
+        }
+
         let tempID = makeid(6);
         let fullID = `${tempID}@testing.com`
 
         it('Email Subscription should work', () => {
-         
+
             // alert(fullID)
             cy.get('#subscriptionEmail').type(`${fullID}`);
             cy.get('.small-4 > input.button').click()
             cy.get('#formWrapper > div > h6').contains('Thank you for your subscription!')
-            
+
         });
 
         it("Same Email can't be used for subscription", () => {
