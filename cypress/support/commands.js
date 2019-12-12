add253e2f858341109729599b802c1f23939efcd
beekimrj <beekimrj@gmail.com>
2019-12-12 16:44:52 +0545

    add custom website user login command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,15 +7,22 @@ Cypress.Commands.add('getLazySrc', (checkattr,location) => {
 })
 
 
-
-
-
-
-
-
-
-
-
+ Cypress.Commands.add('login', () => {
+     var cookie;
+     cy.getCookie('_trip101_session')
+     .then( (cookie) => {
+         if(!cookie)
+         {
+             cy.visit('https://staging.trip101.com/website_users/sign_in')
+             cy.get('#website_user_email')
+             .type(Cypress.env('username'))
+             cy.get('#website_user_password')
+             .type(Cypress.env('password'))
+             cy.get('.actions > input')
+             .click()
+         }
+     })
+ })
 
 
 
