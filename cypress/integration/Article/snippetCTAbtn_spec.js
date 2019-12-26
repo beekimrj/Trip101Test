0e41fb3deb672e09b77c56079a8cf691cc658b6e
beekimrj <beekimrj@yahoo.com>
2019-12-26 15:01:58 +0545

    modify snippetCTA btn, we can add paragraph with snippet number

--- cypress/integration/Article/snippetCTAbtn_spec.js
+++ cypress/integration/Article/snippetCTAbtn_spec.js
@@ -1,28 +1,32 @@
 describe('Article',() => {
     context('Snippet/CTA button', () => {
         before( () => {
-              cy.visit('/article/5-star-hotels-kathmandu')
+            cy.visit(`/article/5-star-hotels-kathmandu`)
+
         })
         
+        //NOTE:: If inserting paragraph doesn't work, there might be other div sections which might be hidden 
+        const paragraph_with_snippet = 2;
+        
     it('paragraph has partner snippet/CTA button, its title should also be hyperlinked with the snippet', () => {
-
-      
-        cy.get('span.paragraphs-decription > section:nth-child(1) > h2')
+        cy.get(`span.paragraphs-decription > section:nth-child(${paragraph_with_snippet}) > h2`)
         .should('have.descendants','a')
-        cy.get('span.paragraphs-decription > section:nth-child(1) > div.one-hotel.landscape.row > .one-para > .hotelsnippet ')
-       .should('exist')
+    cy.get(`span.paragraphs-decription > section:nth-child(${paragraph_with_snippet})`)
+    .should('have.descendants','.snippet-cta')
+   .should('exist')
     })
 
 
     it('the snippet contains the price, title should also contain the same price', () => {
 
         //getting location of price
-        cy.get('span.paragraphs-decription > section:nth-child(1) > div.one-hotel.landscape.row > .one-para > .hotelsnippet  > div.snippet-cta.main_pic_cta.main_pic_cta_new.medium-8.large-5 > a:nth-child(2) > div > div.provider-price.small-4.columns > div > div.price ')
+        cy.get(`span.paragraphs-decription > section:nth-child(${paragraph_with_snippet})`)
+        .find('.price')
         .then(($price) => {
-            const balance = $price.text();
+            const balance = $price.text().trim(); // trim is to remove space before and after the price
             // alert(balance)
 
-            cy.get('span.paragraphs-decription > section:nth-child(1) > h2 > a').should('contain',`${balance}`)
+            cy.get(`span.paragraphs-decription > section:nth-child(${paragraph_with_snippet}) > h2 > a`).should('contain',`${balance}`)
 
         })
     });
